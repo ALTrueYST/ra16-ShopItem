dc644de8cc1efe4268cbd317b686079abc30548e
ALTrueYST <altrueyst@mail.ru>
2026-04-18 21:29:53 +0300

    move `const` up

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@
 import { ClassShop } from './components/cc/ClassShop'
 import './Apps.css'
 
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
 function App() {
   return (
     <div className="container">
@@ -16,15 +25,6 @@ function App() {
       </div>
     </div>
   )
-}
-
-const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
 }
 
 export default App
